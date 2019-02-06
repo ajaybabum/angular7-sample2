bcdf0d5796d891a66c41a3371fb55a6cbabc9c77
ajaybabu534 <ajaymoparthi534@gmail.com>
2019-02-06 19:00:20 +0530

    Sample routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
+import { Sample2Component } from './sample2/sample2.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SampleComponent } from './sample/sample.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",component:SampleComponent},{path:"sample",component:Sample2Component}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { Sample2Component } from './sample2/sample2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    Sample2Component
   ],
   imports: [
     BrowserModule,
